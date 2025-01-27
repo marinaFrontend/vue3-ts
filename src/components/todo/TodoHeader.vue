0939e8f904ad912e83c6b8530e472081a692d5a5
Marina Butova <butova@bobday.ru>
2025-01-27 12:38:06 +0300

    confirm clear all

--- src/components/todo/TodoHeader.vue
+++ src/components/todo/TodoHeader.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="d-flex align-center mb-5">
     <TodoCreateButton class="me-2" @click="$emit('create')"/>
-    <TodoClearButton />
+    <TodoClearButton @click="openConfirmDeleteDialog" />
+    <TodoConfirmDialog
+      v-model="confirmDeleteDialogVisible"
+      title="Удалить все задачи?"
+      button="Да, удалить"
+      @confirm="onClearList"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import TodoCreateButton from "@/components/todo/buttons/TodoCreateButton.vue";
 import TodoClearButton from "@/components/todo/buttons/TodoClearButton.vue";
+import TodoConfirmDialog from '@/components/todo/dialogs/TodoConfirmDialog.vue';
+
+import { useTodoStore } from '@/stores/todo'
+import {ref} from "vue";
+
+const store = useTodoStore();
+
+const confirmDeleteDialogVisible = ref<boolean>(false);
+
+const openConfirmDeleteDialog = () => {
+  confirmDeleteDialogVisible.value = true;
+}
+
+async function onClearList() {
+  await store.clearTodoList();
+  confirmDeleteDialogVisible.value = false;
+}
 </script>
--- src/components/todo/buttons/TodoClearButton.vue
+++ src/components/todo/buttons/TodoClearButton.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-btn v-if="store.data.length" :disabled="store.isFetching || store.isLoading" prepend-icon="mdi-delete-outline" flat @click="onClearList">Очистить список</v-btn>
+  <v-btn
+    v-if="store.data.length"
+    :disabled="store.isFetching || store.isLoading"
+    prepend-icon="mdi-delete-outline"
+    flat
+  >Очистить список</v-btn>
 </template>
 
 <script setup lang="ts">
 import { useTodoStore } from '@/stores/todo'
 
 const store = useTodoStore();
-
-async function onClearList() {
-  await store.clearTodoList()
-}
 </script>
